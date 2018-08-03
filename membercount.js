253f72add2881c73b259f78441bddeece3293bf3
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-08-03 15:42:56 +0100

    Add files via upload

---
+++ membercount.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on("guildMemberAdd", async member => {
+  client.channels.get(CHANNEL_PARA_EDITAR).edit({
+    name: `Members: ${member.guild.members.size}`
+  });
+});
+
+client.on("guildMemberRemove", () => {
+  client.channels.get(CHANNEL_PARA_EDITAR).edit({
+    name: `Members: ${member.guild.members.size}`
+  });
+});
+
+client.prefix(YOUR_TOKEN_HERE);
